2abe2958aefcc42829749d0e709538baca157075
psalire <philip.s.salire@gmail.com>
2021-06-29 03:26:31 -0400

    extract headers

--- burp/BurpExtender.java
+++ burp/BurpExtender.java
@@ -1,84 +1,86 @@
 package burp;
 
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import javax.json.Json;
 
-public class BurpExtender implements IBurpExtender, IHttpListener,
-        IProxyListener, IScannerListener, IExtensionStateListener
-{
+public class BurpExtender implements IBurpExtender, IHttpListener, IScannerListener, IExtensionStateListener {
     private IBurpExtenderCallbacks callbacks;
+    private IExtensionHelpers helpers;
     private PrintWriter stdout;
+    private PrintWriter stderr;
+    private Pattern reHeader;
 
-    //
-    // implement IBurpExtender
-    //
-
+    /**
+    * implement IBurpExtender
+    */
     @Override
-    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks)
-    {
-        // keep a reference to our callbacks object
-        this.callbacks = callbacks;
-
-        // set our extension name
+    public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
         callbacks.setExtensionName("Event listeners");
 
-        // obtain our output stream
-        stdout = new PrintWriter(callbacks.getStdout(), true);
-
-        // register ourselves as an HTTP listener
         callbacks.registerHttpListener(this);
-
-        // register ourselves as a Proxy listener
-        callbacks.registerProxyListener(this);
-
-        // register ourselves as a Scanner listener
         callbacks.registerScannerListener(this);
-
-        // register ourselves as an extension state listener
         callbacks.registerExtensionStateListener(this);
+
+        this.callbacks = callbacks;
+
+        this.stdout = new PrintWriter(callbacks.getStdout(), true);
+        this.stderr = new PrintWriter(callbacks.getStderr(), true);
+
+        this.helpers = callbacks.getHelpers();
+        this.reHeader = Pattern.compile("^(.+): (.+)$");
     }
 
-    //
-    // implement IHttpListener
-    //
-
+    /**
+    * implement IHttpListener
+    */
     @Override
-    public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo)
-    {
-        stdout.println(
-                (messageIsRequest ? "HTTP request to " : "HTTP response from ") +
-                messageInfo.getHttpService() +
-                " [" + callbacks.getToolName(toolFlag) + "]");
+    public void processHttpMessage(int toolFlag, boolean messageIsRequest, IHttpRequestResponse messageInfo) {
+        // stdout.println(
+        //         (messageIsRequest ? "HTTP request to " : "HTTP response from ") +
+        //         messageInfo.getHttpService() +
+        //         " [" + callbacks.getToolName(toolFlag) + "]");
+
+        IRequestInfo requestInfo = helpers.analyzeRequest(messageInfo.getRequest());
+        List<String> headersList = requestInfo.getHeaders();
+
+        stdout.println("Headers: ");
+        stdout.println("(Size: "+headersList.size()+")");
+        stdout.println(headersList);
+        for (int i=1; i<headersList.size(); i++) {
+            String headerStr = headersList.get(i);
+            stdout.println(i+"\""+headerStr+"\"");
+            // stdout.println("  Parsed:");
+            try {
+                Matcher matchHeader = this.reHeader.matcher(headerStr);
+                matchHeader.find();
+                stdout.println("  "+matchHeader.group(1));
+                stdout.println("  "+matchHeader.group(2));
+            }
+            catch (Exception e) {
+                stdout.println("Error on matching: "+headerStr);
+                stderr.println(e);
+                stderr.println(e.getStackTrace());
+            }
+        }
+        stdout.println("--------------------");
     }
 
-    //
-    // implement IProxyListener
-    //
-
+    /**
+    * implement IScannerListener
+    */
     @Override
-    public void processProxyMessage(boolean messageIsRequest, IInterceptedProxyMessage message)
-    {
-        stdout.println(
-                (messageIsRequest ? "Proxy request to " : "Proxy response from ") +
-                message.getMessageInfo().getHttpService());
-    }
-
-    //
-    // implement IScannerListener
-    //
-
-    @Override
-    public void newScanIssue(IScanIssue issue)
-    {
+    public void newScanIssue(IScanIssue issue) {
         stdout.println("New scan issue: " + issue.getIssueName());
     }
 
-    //
-    // implement IExtensionStateListener
-    //
-
+    /**
+    * implement IExtensionStateListener
+    */
     @Override
-    public void extensionUnloaded()
-    {
+    public void extensionUnloaded() {
         stdout.println("Extension was unloaded");
     }
 }
