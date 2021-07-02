e442e0a40c797918ee1bbac9ccb65979e99c8dbf
psalire <philip.s.salire@gmail.com>
2021-07-01 20:14:17 -0400

    paths insert

--- src/ts/server/PathsTable.ts
+++ src/ts/server/PathsTable.ts
@@ -7,34 +7,47 @@ export default class PathsTable extends aSqliteTable {
     private hostsModel: ModelCtor<Model>;
 
     constructor(model: ModelCtor<Model>, hostsModel: ModelCtor<Model>) {
-        super(model, ['path']);
+        super(model, ['path', 'HostId']);
         this.hostsModel = hostsModel;
     }
-
     public async insert(vals: string[], host?: string): Promise<any> {
         if (host === undefined) {
-            throw new SqliteDatabaseError(
-                'PathsTable.insert(vals, host): missing host argument'
-            );
+            throw new SqliteDatabaseError('missing host argument');
         }
         super.validateValuesLength(vals);
+
         var hostObj = await this.hostsModel.findOne({
             where: {
                 host: host
             }
         });
         if (hostObj == null) {
-            throw new SqliteDatabaseError(`PathsTable.insert(): cannot find host ${host}`)
+            throw new SqliteDatabaseError(`cannot find host "${host}"`);
         }
         return this.model.create({
             path: vals[0]
         }).then((createdPath) => {
-            createdPath.setHost(hostObj)
+            return createdPath.setHost(hostObj);
         });
     }
-    public bulkInsert(vals: string[][]): Promise<any> {
+    public async bulkInsert(vals: string[][], host?: string): Promise<any> {
+        if (host === undefined) {
+            throw new SqliteDatabaseError('missing host argument');
+        }
+
+        var hostObj = await this.hostsModel.findOne({
+            where: {
+                host: host
+            }
+        });
+
         return this.model.bulkCreate(vals.flat().map((val) => {
-            return {path: val}
-        }));
+            return {path: val};
+        })).then((createdObjs) => {
+            for (let obj of createdObjs) {
+                obj.setHost(hostObj);
+            }
+            return createdObjs;
+        });
     }
 }
--- test/ts/server/SequelizeSqlite.test.ts
+++ test/ts/server/SequelizeSqlite.test.ts
@@ -1,5 +1,6 @@
 
 import SqliteDatabase from "../../../src/ts/server/SqliteDatabase";
+import SqliteDatabaseError from "../../../src/ts/server/SqliteDatabaseError";
 import * as fs from "fs";
 import * as path from "path";
 
@@ -97,6 +98,27 @@ test('Insert into paths table', async () => {
     await db.paths.insert(['/'], 'example.com');
     let models = await db.paths.selectAll();
     expect(models.length).toBe(1);
-    console.log(models[0]);
     expect(models[0].path).toBe('/');
+
+    await db.paths.insert(['/index.html'], 'example.com');
+    await db.paths.insert(['/word.exe'], 'example.com');
+    await db.paths.insert(['/a/path/file'], 'test.test5.com');
+    await db.paths.insert(['/home'], 'www.test3.com');
+    models = await db.paths.selectAll();
+    expect(models.length).toBe(5);
+    expect(models[0].path).toBe('/');
+    expect(models[1].path).toBe('/index.html');
+    expect(models[2].path).toBe('/word.exe');
+    expect(models[3].path).toBe('/a/path/file');
+    expect(models[4].path).toBe('/home');
+    expect((await models[0].getHost()).host).toBe('example.com');
+    expect((await models[1].getHost()).host).toBe('example.com');
+    expect((await models[2].getHost()).host).toBe('example.com');
+    expect((await models[3].getHost()).host).toBe('test.test5.com');
+    expect((await models[4].getHost()).host).toBe('www.test3.com');
+
+});
+
+test('Insert path with non-existing host', async () => {
+    await expect(db.paths.insert(['/abcd'], 'google.com')).rejects.toThrow(SqliteDatabaseError);
 });
