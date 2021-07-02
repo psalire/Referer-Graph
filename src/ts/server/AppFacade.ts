2dbb8349c8fdbf3c7755170781f1112e327938ea
psalire <philip.s.salire@gmail.com>
2021-07-02 18:50:23 -0400

    app facade

---
+++ src/ts/server/AppFacade.ts
@@ -0,0 +1,35 @@
+
+import SqliteDatabase from './SqliteDatabase';
+
+export default class AppFacade {
+    private sqliteDb : SqliteDatabase;
+
+    public constructor(dbPath='./sqlite-dbs', dbName='default.sqlite') {
+        this.sqliteDb = new SqliteDatabase(dbPath, dbName);
+    }
+
+    public addHost(host: string): Promise<any> {
+        return this.sqliteDb.hosts.insert([host]);
+    }
+    public addHosts(hosts: string[]): Promise<any> {
+        return this.sqliteDb.hosts.bulkInsert(hosts.map(val=>[val]));
+    }
+
+    public addPath(path: string, host: string): Promise<any> {
+        return this.sqliteDb.paths.insert([path], host);
+    }
+    public addPaths(paths: string[], host: string): Promise<any> {
+        return this.sqliteDb.paths.bulkInsert(paths.map(val=>[val]), host);
+    }
+
+    public addSrcDstMapping(
+        srcDst: string[], srcHost: string, dstHost?: string
+    ): Promise<any> {
+        return this.sqliteDb.srcDsts.insert(srcDst, srcHost, dstHost);
+    }
+    public addSrcDstMappings(
+        srcDsts: string[][], srcHost: string, dstHost?: string
+    ): Promise<any> {
+        return this.sqliteDb.srcDsts.bulkInsert(srcDsts, srcHost, dstHost);
+    }
+}
