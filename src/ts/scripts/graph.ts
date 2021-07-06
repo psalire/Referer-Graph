dd472644c4f2552e59b232f3a3ade36fb2e0a343
psalire <philip.s.salire@gmail.com>
2021-07-06 15:07:40 -0400

    d3 graph

--- src/ts/scripts/graph.ts
+++ src/ts/scripts/graph.ts
@@ -17,7 +17,7 @@ svg = svg.call(d3.zoom().on("zoom", zoomed)).append("g");
 svg.append("defs").append("marker")
     .attr("id", "arrow")
     .attr("viewBox", "0 -5 10 10")
-    .attr("refX", 15)
+    .attr("refX", 20)
     .attr("refY", 0)
     .attr("markerWidth", 8)
     .attr("markerHeight", 8)
@@ -26,16 +26,16 @@ svg.append("defs").append("marker")
     .attr("d", "M0,-5L10,0L0,5");
 
 var color = d3.scaleOrdinal(d3.schemeCategory10);
-
+const radius = 10;
 
 var simulation = d3.forceSimulation()
     .force("link", d3.forceLink().id((d) => { return d.id; }))
-    // .force("charge", d3.forceManyBody().strength(-300))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2))
-    .force("x", d3.forceX())
-    .force("y", d3.forceY())
-// .force("collision", d3.forceCollide(25));
+    .force("charge", d3.forceManyBody().strength(-1000))
+    // .force("charge", d3.forceManyBody())
+    // .force("center", d3.forceCenter(width / 2, height / 2))
+    .force("x", d3.forceX(width / 2))
+    .force("y", d3.forceY(height / 2))
+    .force("collision", d3.forceCollide().radius(radius))
 
 //d3.json("data.json", createGraph );
 
@@ -56,7 +56,7 @@ function createGraph(error, graph) {
         .selectAll("circle")
         .data(graph.nodes)
         .enter().append("circle")
-        .attr("r", 4)
+        .attr("r", radius)
         .attr("fill", (d) => { if (d.root == "true") return color(d.root); return color(d.type); })
         .call(d3.drag()
             .on("start", dragstarted)
@@ -71,7 +71,7 @@ function createGraph(error, graph) {
 
     text.append("text")
         .attr("x", 20)
-        .attr("y", ".31em")
+        // .attr("y", ".31em")
         .style("font-family", "sans-serif")
         .style("font-size", "0.7em")
         .text((d) => { return d.id; });
@@ -80,8 +80,8 @@ function createGraph(error, graph) {
         console.log("clicked", d.id);
     });
 
-    node.append("title")
-        .text((d) => { return d.id; });
+    // node.append("title")
+    //     .text((d) => { return d.id; });
 
     simulation
         .nodes(graph.nodes)
@@ -136,6 +136,7 @@ var data = {
 var isGraphCreated = false;
 
 var knownPaths = new Set();
+var knownPathsIndex = [];
 var knownLinks = new Set();
 
 const socket = io();
@@ -161,6 +162,12 @@ socket.on('data', (msg) => {
         let srcDstStr = src+dst;
         if (src!=dst && !(knownLinks.has(srcDstStr))) {
             knownLinks.add(srcDstStr);
+            let type = knownPathsIndex.indexOf(src);
+            if (type==-1) {
+                knownPathsIndex.push(src);
+                type = knownPathsIndex.indexOf(src);
+            }
+            console.log(type)
             data.links.push({
                 "source": src,
                 "target": dst,
@@ -180,67 +187,65 @@ socket.on('data', (msg) => {
 
 var update = function() {
 
+    // Update the nodes
+    let node = svg.select('.nodes')
+        .selectAll("circle").data(data.nodes);
+    // Exit any old nodes
+    node.exit().remove();
+    // Enter any new nodes
+    node = node.enter().append("circle")
+                    .attr("r", radius)
+                    .attr("fill", (d) => { if (d.root == "true") return color(d.root); return color(d.type); })
+                    .attr("cx", width/2)
+                    .attr("cy", height/2)
+                    .call(d3.drag()
+                        .on("start", dragstarted)
+                        .on("drag", dragged))
+                    .merge(node)
+                    // .append("title")
+                    // .text((d) => { return d.id; });
+
     // Update links
     let link = svg.select('.links')
         .selectAll("line")
         .data(data.links);
-
+    // Exit any old links
+    link.exit().remove();
     // Enter links
-    let linkEnter = link
+    link = link
         .enter().append("line")
         .attr("stroke", (d) => { return color(d.type); })
-        .attr("marker-end", "url(#arrow)");
-
-    // Update the nodes
-    let node = svg.select('.nodes')
-        .selectAll("circle").data(data.nodes);
-
-    // Enter any new nodes
-    let nodeEnter = node.enter().append("circle")
-                    .attr("r", 4)
-                    .attr("fill", (d) => { if (d.root == "true") return color(d.root); return color(d.type); })
-                    .call(d3.drag()
-                        .on("start", dragstarted)
-                        .on("drag", dragged));
-
+        .attr("marker-end", "url(#arrow)")
+        .merge(link);
 
     let text = svg.select('.labels')
                 .selectAll('g')
                 .data(data.nodes);
-    let textEnter = text.enter().append("g")
+    text.exit().remove();
+    text = text.enter().append("g")
                     .append("text")
                     .attr("x", 20)
-                    .attr("y", ".31em")
+                    // .attr("y", 20)
                     .style("font-family", "sans-serif")
                     .style("font-size", "0.7em")
-                    .text((d) => { return d.id; });
-
-    node.append("title")
-        .text((d) => { return d.id; });
-
-    link = linkEnter.merge(link);
-    // Exit any old links
-    link.exit().remove();
-    node = nodeEnter.merge(node);
-    // Exit any old nodes
-    node.exit().remove();
-    text = textEnter.merge(text);
-    text.exit().remove();
+                    .text((d) => { return d.id; })
+                    .merge(text);
+    text = svg.select('.labels')
+               .selectAll('g')
 
     // Redefine and restart simulation
     simulation.nodes(data.nodes)
               .on("tick", ticked);
-
     simulation.force("link")
               .links(data.links);
     simulation.restart();
 
 
     function ticked() {
-        var bbox = document.getElementById("graph").getBoundingClientRect()
-        var width = bbox.width;
-        var height = bbox.height;
-        var radius = 4;
+        // var bbox = document.getElementById("graph").getBoundingClientRect()
+        // var width = bbox.width;
+        // var height = bbox.height;
+        // var radius = 4;
         link
             .attr("x1", (d) => { return d.source.x; })
             .attr("y1", (d) => { return d.source.y; })
