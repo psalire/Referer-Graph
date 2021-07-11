ca5501e4c2553fee006fc4eaad6794ab82503fd8
psalire <philip.salire@sjsu.edu>
2021-07-11 01:37:03 -0700

    Web: create btns for selecting graph style

--- src/ts/scripts/graph.ts
+++ src/ts/scripts/graph.ts
@@ -6,7 +6,7 @@ import { io } from "socket.io-client";
 function createButton(text: string): HTMLButtonElement {
     var btn = document.createElement('button');
     btn.type = 'button';
-    btn.className = 'btn btn-primary mb-1';
+    btn.className = 'btn btn-primary mb-1 graph-controls-btn';
     btn.textContent = text;
     return btn;
 }
