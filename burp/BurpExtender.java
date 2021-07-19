9f35af4b2f2b5c0b3aaa944e70ff5807b9cbc8d9
psalire <philip.salire@sjsu.edu>
2021-07-19 00:42:29 -0700

    Burp: burp tab, change extension name

--- burp/BurpExtender.java
+++ burp/BurpExtender.java
@@ -5,6 +5,9 @@ import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.net.URL;
 import java.net.MalformedURLException;
+import java.awt.Component;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObjectBuilder;
 import javax.json.JsonStructure;
@@ -12,19 +15,22 @@ import javax.json.JsonObject;
 import javax.json.JsonArray;
 import javax.json.Json;
 
-public class BurpExtender implements IBurpExtender, IHttpListener, IExtensionStateListener {
+public class BurpExtender implements IBurpExtender, IHttpListener, ITab, IExtensionStateListener {
     private IBurpExtenderCallbacks callbacks;
     private IExtensionHelpers burpHelpers;
     private Pattern reHeader;
     private Writer writer;
     private HttpHandler httpHandler;
+    private final String extensionName = "Referer Graph";
+
+    private JPanel uiPanel;
 
     /**
     * implement IBurpExtender
     */
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
-        callbacks.setExtensionName("Requests Visualizer");
+        callbacks.setExtensionName(this.extensionName);
         callbacks.registerHttpListener(this);
         callbacks.registerExtensionStateListener(this);
 
@@ -33,6 +39,33 @@ public class BurpExtender implements IBurpExtender, IHttpListener, IExtensionSta
         this.reHeader = Pattern.compile("^(.+): (.+)$");
         this.writer = new Writer(callbacks.getStdout(), callbacks.getStderr());
         this.httpHandler = new HttpHandler(this.writer);
+
+        SwingUtilities.invokeLater(new Runnable()
+        {
+            @Override
+            public void run() {
+                uiPanel = new JPanel();
+
+                callbacks.customizeUiComponent(uiPanel);
+
+                // add the custom tab to Burp's UI
+                callbacks.addSuiteTab(BurpExtender.this);
+            }
+        });
+    }
+
+    /**
+    * implement ITab
+    */
+    @Override
+    public String getTabCaption()
+    {
+        return this.extensionName;
+    }
+    @Override
+    public Component getUiComponent()
+    {
+        return this.uiPanel;
     }
 
     /**
