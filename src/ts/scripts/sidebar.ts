0a7fe81fed49ad7eea136fed5646a7ff99b76198
psalire <philip.s.salire@gmail.com>
2021-08-10 14:12:57 -0400

    Web: fix select not hiding

--- src/ts/scripts/sidebar.ts
+++ src/ts/scripts/sidebar.ts
@@ -4,10 +4,10 @@ import ToggleElement from './ToggleElement';
 window.addEventListener("graphLoaded", () => {
     var header = document.querySelector('#sidebar .settings-header-label .control-text');
     var btns = new ToggleElement(document.getElementById('buttons'));
-    btns.addToggleValue('hide', 'hide-buttons', 'show-buttons');
+    btns.addToggleValue('hide', 'hide-components', 'show-components');
     document.getElementById('sidebar-toggle').addEventListener('click', function() {
         btns.toggleStyle('hide');
-        if (btns.getElem().classList.contains('hide-buttons')) {
+        if (btns.getElem().classList.contains('hide-components')) {
             header.textContent = '';
         }
         else {
